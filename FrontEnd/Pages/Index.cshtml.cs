82e7870c1dda51053edca735d0f5dbc40c070f4b
min.san <min.san@DDMAILSHPDEV12>
2020-05-12 16:14:10 +0800

    More main dashboard changes

--- FrontEnd/Pages/Index.cshtml.cs
+++ FrontEnd/Pages/Index.cshtml.cs
@@ -27,7 +27,10 @@ namespace FrontEnd.Pages
         public List<Questions> _mostratedqn2 { get; set; }
 
         [BindProperty]
-        public List<Answers> _mostratedans { get; set; }
+        public List<Answers> _mostratedans1 { get; set; }
+
+        [BindProperty]
+        public List<Answers> _mostratedans2 { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -44,7 +47,8 @@ namespace FrontEnd.Pages
 
             _mostratedqn1 = await _apiClient.GetTopRatedQuestions();
             _mostratedqn2 = await _apiClient.GetTopRatedQuestions(5, 30);
-            _mostratedans = await _apiClient.GetTopRatedAnswers();
+            _mostratedans1 = await _apiClient.GetTopRatedAnswers();
+            _mostratedans2 = await _apiClient.GetTopRatedAnswers(5, 30);
 
             _currentUser = JsonConvert.DeserializeObject<Users>(Request.Cookies["CurrentUser"]);
             // Info.  
